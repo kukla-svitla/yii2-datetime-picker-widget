0630c04e8c71ce7a156267f4e2c039d55c4124d6
Ivan Pushkin <imetalguardi@gmail.com>
2015-04-14 18:35:03 +0300

    update api and readme

--- README.md
+++ README.md
@@ -26,8 +26,55 @@ to the require section of your `composer.json` file.
 Usage
 -----
 
-Once the extension is installed, simply use it in your code by  :
+This widget renders a input control with datetime picker.
+
+You can use is like a separate widget or with `ActiveField` instance.
 
 ```php
 
+<?= \metalguardian\dateTimePicker\Widget::widget([
+
+    // you need specify model and attribute
+
+    'model' => $model,
+    'attribute' => 'posted',
+
+    // or name of the input and value (if needed)
+
+    //'value' => '2016/01/22 18:26',
+    //'name' => 'specific-name',
+
+    'mode' => \metalguardian\dateTimePicker\Widget::MODE_DATE,
+    // for only date picker
+    // or \metalguardian\dateTimePicker\Widget::MODE_TIME for time picker
+    // default is datetime picker
+
+    'language' => 'ru',
+    'options' => [ // html options of the input
+        'class' => 'my-class',
+    ],
+    'clientOptions' => [
+        'theme' => 'dark',
+        'minDate' => '05.12.2013',
+        'formatDate' => 'd.m.Y',
+    ]
+]) ?>
+
+<?= $form->field($model, 'start_time')->widget(\metalguardian\dateTimePicker\Widget::className(), [
+    'language' => 'ru',
+    'mode' => \metalguardian\dateTimePicker\Widget::MODE_TIME,
+    'options' => [
+        'class' => 'my-class',
+    ],
+    'clientOptions' => [
+        'theme' => 'dark',
+        'minDate' => '05.12.2013',
+        'formatDate' => 'd.m.Y',
+    ]
+]) ?>
 ```
+
+License
+-------
+
+**yii2-datetime-picker-widget** is released under the MIT License. See the bundled `LICENSE` for details.
--- src/Widget.php
+++ src/Widget.php
@@ -2,6 +2,11 @@
 
 namespace metalguardian\dateTimePicker;
 
+use Yii;
+use yii\base\InvalidParamException;
+use yii\helpers\FormatConverter;
+use yii\helpers\Html;
+use yii\helpers\Json;
 use yii\widgets\InputWidget;
 
 /**
@@ -10,14 +15,154 @@ use yii\widgets\InputWidget;
  */
 class Widget extends InputWidget
 {
+    const MODE_DATE = 1;
+    const MODE_TIME = 2;
+
+    /**
+     * @var int the mode of the picker: time or date picker. default datetime picker
+     */
+    public $mode;
+
+    /**
+     * @var string the language ID (e.g. 'fr', 'de', 'en-GB') for the language to be used by the datetime picker.
+     * If this property is empty, then the current application language will be used.
+     */
+    public $language;
+
+    /**
+     * @var array the options for the datetime picker widget.
+     * Please refer to the corresponding [jQuery date-time picker page](http://xdsoft.net/jqplugins/datetimepicker/)
+     * for possible options.
+     */
+    public $clientOptions = [];
+
     /**
      * @inheritdoc
      */
     public function init()
     {
         parent::init();
-        //if ($this->dateFormat === null) {
-        //    $this->dateFormat = Yii::$app->formatter->dateFormat;
-        //}
+    }
+
+    protected $languages = [
+        'ar',
+        'az',
+        'bg',
+        'bs',
+        'ca',
+        'ch',
+        'cs',
+        'da',
+        'de',
+        'el',
+        'en',
+        'en-GB',
+        'es',
+        'et',
+        'eu',
+        'fa',
+        'fi',
+        'fr',
+        'gl',
+        'he',
+        'hr',
+        'hu',
+        'id',
+        'it',
+        'ja',
+        'ko',
+        'kr',
+        'lt',
+        'lv',
+        'mk',
+        'mn',
+        'nl',
+        'no',
+        'pl',
+        'pt',
+        'pt-BR',
+        'ro',
+        'ru',
+        'se',
+        'sk',
+        'sl',
+        'sq',
+        'sr',
+        'sr-YU',
+        'sv',
+        'th',
+        'tr',
+        'uk',
+        'vi',
+        'zh',
+        'zh-TW',
+    ];
+
+    protected $fallbackLanguage = 'en';
+
+    /**
+     * Renders the widget.
+     */
+    public function run()
+    {
+        echo $this->renderWidget() . "\n";
+
+        $containerID = $this->options['id'];
+
+        $language = $this->language ? $this->language : Yii::$app->language;
+        if ($this->isAcceptedLanguage($language)) {
+            $this->clientOptions['lang'] = $language;
+        } else {
+            $this->clientOptions['lang'] = $this->fallbackLanguage;
+        }
+
+        $timePicker = true;
+        $datePicker = true;
+        if ($this->mode === static::MODE_DATE) {
+            $timePicker = false;
+        } elseif ($this->mode === static::MODE_TIME) {
+            $datePicker = false;
+        }
+        $this->clientOptions['timepicker'] = $timePicker;
+        $this->clientOptions['datepicker'] = $datePicker;
+
+        $view = $this->getView();
+        Asset::register($view);
+
+        $options = empty($this->clientOptions) ? '' : Json::encode($this->clientOptions);
+        $js = "jQuery('#{$containerID}').datetimepicker({$options});";
+        $view->registerJs($js);
+    }
+
+    /**
+     * Renders the DateTimePicker widget.
+     * @return string the rendering result.
+     */
+    protected function renderWidget()
+    {
+        // get formatted date value
+        if ($this->hasModel()) {
+            $value = Html::getAttributeValue($this->model, $this->attribute);
+        } else {
+            $value = $this->value;
+        }
+        $options = $this->options;
+
+        if ($this->hasModel()) {
+            $input = Html::activeTextInput($this->model, $this->attribute, $options);
+        } else {
+            $input = Html::textInput($this->name, $value, $options);
+        }
+
+        return $input;
+    }
+
+    /**
+     * @param string $language the language ID
+     * @return bool is accepted language
+     */
+    protected function isAcceptedLanguage($language)
+    {
+        return in_array($language, $this->languages, true);
     }
 }
